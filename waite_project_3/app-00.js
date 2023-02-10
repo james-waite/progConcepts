60c8ea293106b2586ed856212e355179e5cf1f4f
james-waite-cia <jwaite@cia.edu>
2023-02-10 12:36:09 -0500

    vertices tests

---
+++ waite_project_3/app-00.js
@@ -0,0 +1,30 @@
+let vertices = [];
+
+function setup() {
+  myCanvas = createCanvas(500, 500);
+  myCanvas.parent('myContainer');
+}
+
+function draw() {
+  background(150);
+  noFill();
+
+  // Draw shape using the current vertices array
+  beginShape();
+  for (let i = 0; i < vertices.length; i++) {
+    vertex(vertices[i].x, vertices[i].y);
+    endShape(CLOSE);
+  }
+  fill(255, 0, 0);
+  // Draw a circle at all the vertices
+  for (let i = 0; i < vertices.length; i++) {
+    circle(vertices[i].x, vertices[i].y, 15);
+  }
+}
+
+function mouseClicked() {
+  // Update the vertices array with
+  // current mouse position
+  vertices.push({ x: mouseX, y: mouseY });
+  console.log(vertices);
+}
---
+++ waite_project_3/app-01.js
@@ -0,0 +1,74 @@
+let transAmount = 50;
+
+function setup() {
+  myCanvas = createCanvas(500, 500);
+  myCanvas.parent('myContainer');
+}
+
+function draw() {
+  background(150);
+
+  drawShape();
+
+  // Tool to display cursor location, minus the amount translated
+  text(
+    '(' +
+      floor(mouseX - transAmount) +
+      ', ' +
+      floor(mouseY - transAmount) +
+      ')',
+    mouseX,
+    mouseY
+  );
+}
+
+function drawShape() {
+  push();
+  translate(transAmount, transAmount);
+  stroke(255, 0, 0);
+  beginShape();
+  // Exterior, clockwise -- starting w/ top-left
+  vertex(-40, -40);
+
+  /* Starting here, uncomment the code to see which issue is being produced, then comment it back out and work down to the next test.
+  Regular vertex corner, top-right */
+  vertex(80, -40);
+
+  /* Two vertices for a 'bevel' corner */
+  // vertex(70, -40);
+  // vertex(80, -30);
+
+  /* Unused test line, top. beginShape() doesn't appear to like lines and won't include it when closing the shape. May only accept vertices not shapes... */
+  // line(-40, -40, 70, -40);
+
+  /* Four curveVertex() to create curve, first and last are control points, middle two are the 'visible' vertices. The issue is that startShape() contorts all other vertices into curves when these are included, and it also doesn't connect from the first vertex() to the first curveVertex().
+  https://github.com/processing/p5.js/issues/906 
+  https://p5js.org/learn/curves.html
+  */
+  // curveVertex(40, -40);
+  // curveVertex(70, -40);
+  // curveVertex(80, -30);
+  // curveVertex(80, 10);
+
+  /* curve() combines the four curveVertex() into one expression. beginShape() appears to still draw a direct line between middle vertices as well as the curve. It also draws the control points as vertices to the canvas which it isn't supposed to do? It also disregards the first vertex and instead uses the first control point??? */
+  // curve(40, -40, 70, -40, 80, -30, 80, 10);
+
+  vertex(80, 80);
+  vertex(-40, 80);
+  // Interior, counter-clockwise
+  beginContour();
+  vertex(-20, -20);
+  vertex(-20, 20);
+  vertex(20, 20);
+  vertex(20, -20);
+  endContour();
+  // Second interior, counter-clockwise
+  beginContour();
+  vertex(30, 30);
+  vertex(30, 60);
+  vertex(60, 60);
+  vertex(60, 30);
+  endContour();
+  endShape(CLOSE);
+  pop();
+}
