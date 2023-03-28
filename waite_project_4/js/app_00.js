5a64b3d372839e2f2bf1a17ac011fdf13c7b5bfc
james-waite-cia <jwaite@cia.edu>
2023-03-28 14:29:49 -0400

    animController added

--- waite_project_4/js/app_00.js
+++ waite_project_4/js/app_00.js
@@ -1,9 +1,17 @@
-let x1, y1, x2, y2, x3, y3, x4, y4;
+// let x1, y1, x2, y2, x3, y3, x4, y4;
+// let shapeOne = [50, 50, 75, 55, 75, 45, 100, 50];
+// let shapeTwo = [100, 50, 95, 75, 105, 75, 150, 50];
+// let shapeThree = [75, 75, 50, 75, 85, 25, 125, 50];
 let lerpVal = 0.0;
 let lerpAmount = 0.01;
+let beginState = 0;
+let endState = 1;
 let lerpArr = [];
-let shapeOne = [50, 50, 75, 55, 75, 45, 100, 50];
-let shapeTwo = [100, 50, 95, 75, 105, 75, 150, 50];
+let shapes = [
+  [50, 50, 75, 55, 75, 45, 100, 50],
+  [100, 50, 95, 75, 105, 75, 150, 50],
+  [75, 75, 50, 75, 85, 25, 125, 50],
+];
 
 function setup() {
   myCanvas = createCanvas(500, 500);
@@ -12,13 +20,9 @@ function setup() {
 
 function draw() {
   background(150);
-
-  for (let i = 0; i < shapeOne.length; i++) {
-    lerpArr[i] = lerp(shapeOne[i], shapeTwo[i], lerpVal);
-  }
-  lerpVal > 1 || lerpVal < 0 ? (lerpAmount *= -1) : null;
-  lerpVal += sin(lerpAmount) * 0.8;
-  drawShapes(lerpArr);
+  // lerpPoint(shapeOne, shapeTwo, lerpVal);
+  // drawShapes(lerpArr);
+  animController();
 }
 
 function drawShapes(arr) {
@@ -33,142 +37,28 @@ function drawShapes(arr) {
   pop();
 }
 
-// function lerpPoint(one, two, t) {
-//   return lerp(one, two, t);
-// }
-
-/*
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-  
-  // Define two sets of Bezier curve points
-  let b1 = [50, 100, 150, 300, 250, 100, 350, 300];
-  let b2 = [50, 300, 150, 100, 250, 300, 350, 100];
-  
-  // Calculate points along each curve
-  let points1 = [];
-  let points2 = [];
-  for (let t = 0; t <= 1; t += 0.01) {
-    points1.push({
-      x: bezierPoint(b1[0], b1[2], b1[4], b1[6], t),
-      y: bezierPoint(b1[1], b1[3], b1[5], b1[7], t)
-    });
-    points2.push({
-      x: bezierPoint(b2[0], b2[2], b2[4], b2[6], t),
-      y: bezierPoint(b2[1], b2[3], b2[5], b2[7], t)
-    });
-  }
-  
-  // Interpolate between the two sets of points
-  let blendFactor = mouseX / width; // Use mouseX position as blend factor
-  let blendedPoints = [];
-  for (let i = 0; i < points1.length; i++) {
-    let p1 = points1[i];
-    let p2 = points2[i];
-    blendedPoints.push({
-      x: lerp(p1
-*/
-/*
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-
-  // Define the two bezier curves
-  let x1 = 100;
-  let y1 = 100;
-  let x2 = 300;
-  let y2 = 300;
-  let cx1 = 150;
-  let cy1 = 50;
-  let cx2 = 250;
-  let cy2 = 350;
-
-  // Draw the first bezier curve
-  noFill();
-  stroke(0);
-  bezier(x1, y1, cx1, cy1, cx2, cy2, x2, y2);
-
-  // Draw the second bezier curve
-  let x3 = 100;
-  let y3 = 300;
-  let x4 = 300;
-  let y4 = 100;
-  let cx3 = 150;
-  let cy3 = 350;
-  let cx4 = 250;
-  let cy4 = 50;
-  bezier(x3, y3, cx3, cy3, cx4, cy4, x4, y4);
-
-  // Interpolate between the two bezier curves
-  noFill();
-  stroke(255, 0, 0);
-  for (let t = 0; t <= 1; t += 0.01) {
-    let x = bezierPoint(x1, cx1, cx2, x2, t) * (1 - t) + bezierPoint(x3, cx3, cx4, x4, t) * t;
-    let y = bezierPoint(y1, cy1, cy2, y2, t) * (1 - t) + bezierPoint(y3, cy3, cy4, y4, t) * t;
-    ellipse(x, y, 5);
+function lerpPoint(one, two, t) {
+  for (let i = 0; i < one.length; i++) {
+    lerpArr[i] = lerp(one[i], two[i], t);
   }
 }
-*/
-/*
-function interpolateBezierCurves(curve1, curve2, numPoints) {
-  // Extract control points for the first curve
-  let x1 = curve1.start.x;
-  let y1 = curve1.start.y;
-  let cx1 = curve1.control1.x;
-  let cy1 = curve1.control1.y;
-  let cx2 = curve1.control2.x;
-  let cy2 = curve1.control2.y;
-  let x2 = curve1.end.x;
-  let y2 = curve1.end.y;
 
-  // Extract control points for the second curve
-  let x3 = curve2.start.x;
-  let y3 = curve2.start.y;
-  let cx3 = curve2.control1.x;
-  let cy3 = curve2.control1.y;
-  let cx4 = curve2.control2.x;
-  let cy4 = curve2.control2.y;
-  let x4 = curve2.end.x;
-  let y4 = curve2.end.y;
-
-  // Interpolate between the two bezier curves
-  let points = [];
-  for (let i = 0; i <= numPoints; i++) {
-    let t = i / numPoints;
-    let x = bezierPoint(x1, cx1, cx2, x2, t) * (1 - t) + bezierPoint(x3, cx3, cx4, x4, t) * t;
-    let y = bezierPoint(y1, cy1, cy2, y2, t) * (1 - t) + bezierPoint(y3, cy3, cy4, y4, t) * t;
-    points.push({ x: x, y: y });
+function animController() {
+  if (lerpVal >= 1) {
+    beginState++;
+    endState++;
+    lerpVal = 0;
   }
-  return points;
+  if (endState >= shapes.length) {
+    console.log('endState: ' + endState + ' shapes.length: ' + shapes.length);
+    endState === 0;
+  }
+  if (beginState >= shapes.length) {
+    beginState === 0;
+  }
+  console.log('beginState: ' + beginState + ', endState: ' + endState);
+  lerpPoint(shapes[beginState], shapes[endState], lerpVal);
+  drawShapes(lerpArr);
+  // lerpVal > 1 || lerpVal < 0 ? (lerpAmount *= -1) : null;
+  lerpVal += lerpAmount;
 }
-*/
-/*
-let curve1 = {
-  start: { x: 100, y: 100 },
-  control1: { x: 150, y: 50 },
-  control2: { x: 250, y: 350 },
-  end: { x: 300, y: 300 },
-};
-
-let curve2 = {
-  start: { x: 100, y: 300 },
-  control1: { x: 150, y: 350 },
-  control2: { x: 250, y: 50 },
-  end: { x: 300, y: 100 },
-};
-
-let points = interpolateBezierCurves(curve1, curve2, 50);
-
-// draw points on canvas
-for (let i = 0; i < points.length; i++) {
-  let p = points[i];
-  ellipse(p.x, p.y, 5);
-}
-*/
