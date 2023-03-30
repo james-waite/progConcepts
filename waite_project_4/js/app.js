e2cb1657aded493f7f159e001e7a2d32a0608270
james-waite <this.jameswaite@outlook.com>
2023-03-30 01:31:41 -0400

    lerp changes

--- waite_project_4/js/app.js
+++ waite_project_4/js/app.js
@@ -1,11 +1,5 @@
-let transAmount = 0;
 let data = {};
 let shapes = [];
-let shape = [
-  [50, 50, 75, 55, 75, 45, 100, 50],
-  [100, 50, 95, 75, 105, 75, 150, 50],
-  [75, 75, 50, 75, 85, 25, 125, 50],
-];
 
 function preload() {
   data = loadJSON("./json/paths_02.json");
@@ -19,13 +13,15 @@ function loadData() {
     // console.log(pathData); //log to show it's an object
     const pathLength = Object.keys(pathData).length; // unused, but can determine how many arrays are in each shape
     const arr = Object.values(pathData); // extract objects' key values into an array
-    console.log(arr); //log to show it's now an array
-    shapes.push(new Shape(arr));
+    // console.log(arr); //log to show it's now an array
+    // shapes.push(new Shape(arr));
+    shapes.push(arr);
+    // console.log(shapes);
   }
 }
 
 function setup() {
-  myCanvas = createCanvas(500, 500);
+  myCanvas = createCanvas(1000, 1000);
   myCanvas.parent("myContainer");
   loadData();
 }
@@ -33,58 +29,82 @@ function setup() {
 function draw() {
   background(150);
   // loop through shapes[] and calls each display() method
-  for (let i = 0; i < shapes.length; i++) {
-    shapes[i].display();
-  }
+  // for (let i = 0; i < shapes.length; i++) {
+  //   shapes[i].display();
+  // }
   animController();
   helperCoordinates();
 }
 
-class Shape {
-  constructor(data) {
-    this.data = data;
-  }
-  display() {
-    // console.log(this.data);
-    let hasContour = false;
-    push();
-    stroke("red");
-    beginShape();
-    // iterates through each nested array w/in this.data
-    for (const path in this.data) {
-      // console.log(`${this.data[path]}`);
-      // assigns current path object to an array
-      const arr = this.data[path];
-      // console.log(arr);
-      if (!arr[arr.length - 1] && arr.length == 3) {
-        vertex(arr[0], arr[1]);
-      } else if (!arr[arr.length - 1]) {
-        bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
-      } else if (arr.length == 3) {
-        if (hasContour) endContour(CLOSE);
-        if (!hasContour) hasContour = true;
-        beginContour();
-        vertex(arr[0], arr[1]);
-      } else {
-        bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
-      }
+function drawShapes(lerpArr) {
+  let hasContour = false;
+  push();
+  stroke("red");
+  beginShape();
+  for (const path in lerpArr) {
+    const arr = lerpArr[path];
+
+    if (!arr[arr.length - 1] && arr.length == 3) {
+      vertex(arr[0], arr[1]);
+    } else if (!arr[arr.length - 1]) {
+      bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+    } else if (arr.length == 3) {
+      if (hasContour) endContour(CLOSE);
+      if (!hasContour) hasContour = true;
+      beginContour();
+      vertex(arr[0], arr[1]);
+    } else {
+      bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
     }
-    if (hasContour) endContour(CLOSE);
-    endShape(CLOSE);
-    pop();
   }
+  if (hasContour) endContour(CLOSE);
+  endShape(CLOSE);
+  pop();
 }
 
+// class Shape {
+//   constructor(data) {
+//     this.data = data;
+//   }
+//   display() {
+//     // console.log(this.data);
+//     let hasContour = false;
+//     push();
+//     stroke("red");
+//     beginShape();
+//     // iterates through each nested array w/in this.data
+//     for (const path in this.data) {
+//       // console.log(`${this.data[path]}`);
+//       // assigns current path object to an array
+//       const arr = this.data[path];
+//       // console.log(arr);
+//       if (!arr[arr.length - 1] && arr.length == 3) {
+//         vertex(arr[0], arr[1]);
+//       } else if (!arr[arr.length - 1]) {
+//         bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+//       } else if (arr.length == 3) {
+//         if (hasContour) endContour(CLOSE);
+//         if (!hasContour) hasContour = true;
+//         beginContour();
+//         vertex(arr[0], arr[1]);
+//       } else {
+//         bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+//       }
+//     }
+//     if (hasContour) endContour(CLOSE);
+//     endShape(CLOSE);
+//     pop();
+//   }
+// }
+
 function helperCoordinates() {
-  text(
-    "(" +
-      floor(mouseX - transAmount) +
-      ", " +
-      floor(mouseY - transAmount) +
-      ")",
-    mouseX,
-    mouseY
-  );
+  push();
+  translate(-50, 0);
+  stroke(255);
+  strokeWeight(4);
+  textSize(24);
+  text("(" + floor(mouseX) + ", " + floor(mouseY) + ")", mouseX, mouseY);
+  pop();
 }
 
 /*
@@ -95,4 +115,60 @@ function helperCoordinates() {
   x2, y2 -->  first control point
   x3, y3 --> second control point
   x4, y4 -->         anchor point
+  */
+/*
+  "2": [
+    [0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, 0, 0, 0, 0, false],
+    [0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true],
+    [0, 0, 0, 0, 0, 0, true]
+  ]
   */
--- waite_project_4/js/lerp.js
+++ waite_project_4/js/lerp.js
@@ -9,45 +9,47 @@ let lerpArr = [];
 //   [75, 75, 50, 75, 85, 25, 125, 50],
 // ];
 
-// function setup() {
-//   myCanvas = createCanvas(500, 500);
-//   myCanvas.parent("myContainer");
+// function drawShapes(arr) {
+//   push();
+//   beginShape();
+//   noFill();
+//   strokeWeight(4);
+//   stroke("red");
+//   vertex(arr[0], arr[1]);
+//   bezierVertex(arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]);
+//   endShape();
+//   pop();
 // }
-
-// function draw() {
-//   background(150);
-//   // animController();
-// }
-
-function drawShapes(arr) {
-  push();
-  beginShape();
-  noFill();
-  strokeWeight(4);
-  stroke("red");
-  vertex(arr[0], arr[1]);
-  bezierVertex(arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]);
-  endShape();
-  pop();
-}
 
 function lerpPoint(one, two, t) {
+  lerpArr = [];
   for (let i = 0; i < one.length; i++) {
-    lerpArr[i] = lerp(one[i], two[i], t);
+    lerpArr.push([]);
+    for (let j = 0; j < one[i].length; j++) {
+      lerpArr[i].push([]);
+      if (one[i][j] == true || one[i][j] == false) {
+        lerpArr[i][j] = one[i][j];
+      } else {
+        lerpArr[i][j] = Math.floor(lerp(one[i][j], two[i][j], t));
+      }
+    }
   }
 }
 
 function animController() {
+  // when lerpVal goes from 0 --> 1, increment to next shape state and reset lerpVal to 0
   if (lerpVal >= 1) {
     beginState++;
     endState++;
     lerpVal = 0;
   }
-  endState >= shape.length ? (endState = 0) : null;
-  beginState >= shape.length ? (beginState = 0) : null;
-  // console.log("beginState: " + beginState + ", endState: " + endState);
-  lerpPoint(shape[beginState], shape[endState], lerpVal);
-  drawShapes(lerpArr);
-  // lerpVal > 1 || lerpVal < 0 ? (lerpAmount *= -1) : null;
+  // when either end or beginning states are longer than number of shapes, reset back to 0
+  endState >= shapes.length ? (endState = 0) : null;
+  beginState >= shapes.length ? (beginState = 0) : null;
+  // call lerpPoint function, passing current lerpVal and current shape states
+  lerpPoint(shapes[beginState], shapes[endState], lerpVal);
+  // console.log(lerpArr);
+  drawShapes(lerpArr); // draws current shape in lerpArr, created with lerpPoint()
+  // lerpVal > 1 || lerpVal < 0 ? (lerpAmount *= -1) : null; // oscillate between 0 and 1, used for preliminary testing
   lerpVal += lerpAmount;
 }
