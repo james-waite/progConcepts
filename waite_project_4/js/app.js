ba4703985fc7fe14fe5629a076f08a4a0320d411
james-waite-cia <jwaite@cia.edu>
2023-04-30 13:45:22 -0400

    background changes

--- waite_project_4/js/app.js
+++ waite_project_4/js/app.js
@@ -11,7 +11,7 @@ let beginState = 0;
 let endState = 1;
 
 function preload() {
-  data = loadJSON("./json/paths_02.json");
+  data = loadJSON('./json/paths_02.json');
   // simpleShader = loadShader("./shaders/basic.vert", "./shaders/basic.frag");
 }
 
@@ -29,25 +29,25 @@ function loadData() {
 }
 
 function setup() {
-  myCanvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  myCanvas.parent("myContainer");
+  myCanvas = createCanvas(windowWidth, windowHeight);
+  myCanvas.parent('myContainer');
   loadData();
-  sg = createGraphics(1000, 1000);
-
-  radio = createRadio().parent("myContainer").position(50, 25).class("radio");
+  // sg = createGraphics(1000, 1000);
+  background(0);
+  radio = createRadio().parent('myContainer').position(50, 25).class('radio');
   // .option([contentValue],[value]) --> if one param, is both content and value, treated as string
-  radio.option(1, "linearTween");
-  radio.option(2, "easeInQuad");
-  radio.option(3, "easeOutQuad");
-  radio.option(4, "easeInOutQuad");
-  radio.option(5, "easyEase");
+  radio.option(1, 'linearTween');
+  radio.option(2, 'easeInQuad');
+  radio.option(3, 'easeOutQuad');
+  radio.option(4, 'easeInOutQuad');
+  radio.option(5, 'easyEase');
   // set init value of radio
-  radio.value("1");
+  radio.value('1');
 }
 
 function draw() {
   if (paused) return;
-  background(25);
+  background(0, 0, 0, 4);
   animController();
   // helperCoordinates();
 }
--- waite_project_4/js/drawShapes.js
+++ waite_project_4/js/drawShapes.js
@@ -2,33 +2,34 @@
   This is the same code from the prior project, just taken out of the Shape class and into its own function.
 */
 function drawShapes(lerpArr) {
-  sg.clear();
+  // clear();
   let hasContour = false;
-  sg.push();
-  sg.stroke("red");
-  sg.strokeWeight(4);
-  sg.fill(150);
-  sg.scale(0.8); //temporary scaling to fit the window
-  sg.beginShape();
+  push();
+  stroke('red');
+  strokeWeight(4);
+  noFill();
+  // fill(150);
+  scale(0.8); //temporary scaling to fit the window
+  beginShape();
   for (const path in lerpArr) {
     const arr = lerpArr[path];
 
     if (!arr[arr.length - 1] && arr.length == 3) {
-      sg.vertex(arr[0], arr[1]);
+      vertex(arr[0], arr[1]);
     } else if (!arr[arr.length - 1]) {
-      sg.bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+      bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
     } else if (arr.length == 3) {
-      if (hasContour) sg.endContour(CLOSE);
+      if (hasContour) endContour(CLOSE);
       if (!hasContour) hasContour = true;
-      sg.beginContour();
-      sg.vertex(arr[0], arr[1]);
+      beginContour();
+      vertex(arr[0], arr[1]);
     } else {
-      sg.bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+      bezierVertex(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
     }
   }
-  if (hasContour) sg.endContour(CLOSE);
-  sg.endShape(CLOSE);
-  sg.pop();
+  if (hasContour) endContour(CLOSE);
+  endShape(CLOSE);
+  pop();
   // shader(simpleShader);
-  image(sg, -width / 2, -height / 2);
+  // image(sg, -width / 2, -height / 2);
 }
